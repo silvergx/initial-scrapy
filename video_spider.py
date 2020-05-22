48408e0e75d7b6769ebdaa5157691d840f0879bf
silvergx <silvergx34@gmail.com>
2020-05-22 20:34:45 +0800

    update annotate

--- video_spider.py
+++ video_spider.py
@@ -5,6 +5,7 @@ import vid_generator
 
 Stat_Url = "https://api.bilibili.com/x/web-interface/archive/stat?aid="
 
+# need login cookie
 headers = {
     "bfe_id": "fdfaf33a01b88dd4692ca80f00c2de7f",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
